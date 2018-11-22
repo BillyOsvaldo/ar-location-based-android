1801da3e80349c46be5e82a644df632b549db171
bjsetiawan <bill.jodh@gmail.com>
2018-11-22 10:48:43 +0700

    fix bug android

--- app/src/main/java/ng/dat/ar/AROverlayView.java
+++ app/src/main/java/ng/dat/ar/AROverlayView.java
@@ -34,8 +34,12 @@ public class AROverlayView extends View {
 
         //Demo points
         arPoints = new ArrayList<ARPoint>() {{
-            add(new ARPoint("Sun Wheel", 16.0404856, 108.2262447, 0));
-            add(new ARPoint("Linh Ung Pagoda", 16.1072989, 108.2343984, 0));
+            add(new ARPoint("Alun - Alun Purbalingga", -7.39037, 109.3622651, 0));
+            add(new ARPoint("BRI Kaligondang", -7.3782339, 109.3798998, 0));
+            add(new ARPoint("Es Duren Bu Ibar", -7.3909416, 109.3714684, 0));
+            add(new ARPoint("Mie Ayam Rawuh", -7.3833161, 109.3086408, 0));
+            add(new ARPoint("Desa Wisata Purbayasa", -7.3729314, 109.3137048, 0));
+            add(new ARPoint("Owabong Purbaligga", -7.3488852, 109.3474883, 0));
         }};
     }
 
